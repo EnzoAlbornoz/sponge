c6417350693f1ecfcfd068750fead7fe4ee35858
Enzo Albornoz <enzocoelhoalbornoz@gmail.com>
2020-11-21 01:51:46 -0300

    Added Client list command

--- src/main/java/br/ufsc/sponge/client/App.java
+++ src/main/java/br/ufsc/sponge/client/App.java
@@ -1,55 +1,84 @@
-// package br.ufsc.sponge.client;
+package br.ufsc.sponge.client;
 
-// // import java.io.IOException;
-// // import java.net.Authenticator;
-// // import java.net.InetSocketAddress;
-// // import java.net.ProxySelector;
-// // import java.net.URI;
-// // import java.net.URISyntaxException;
-// // import java.net.http.*;
-// // import java.net.http.HttpResponse.BodyHandlers;
-// // import java.time.Duration.*;
+import picocli.CommandLine;
+import picocli.CommandLine.*;
+import picocli.CommandLine.Model.CommandSpec;
 
-// import ch.jalu.*;
-// import kong.unirest.HttpResponse;
-// import kong.unirest.JsonNode;
-// import kong.unirest.Unirest;
-// import java.io.File;
-// import java.io.IOException;
-// import java.nio.file.Files;
-// import io.airlift.airline.*;
-// import io.airlift.airline.Cli.CliBuilder;
-// import io.airlift.airline.model.*;
-// public class App {
-//     public static void main(String[] args) throws IOException {
-//         // Initialize from Settings
-//         // var unirest = Unirest.primaryInstance();
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONElement;
+import kong.unirest.json.JSONObject;
+@Command(name = "sponge-client", description = "Files Backup tool")
+public class App implements Runnable {
+    // Save CLI reference
+    @Spec CommandSpec spec;
+    // Global Options
+    @Option(names = {"-H", "--host"}, description = "Hostname", defaultValue = "localhost", scope = ScopeType.INHERIT)
+    String host;
+    @Option(names = {"-P", "--port"}, description = "Port", defaultValue = "9647", scope = ScopeType.INHERIT)
+    int port;
+    // Commands
+    @Command(
+        name = "list",
+        description = "List all files present on backup storage"
+    )
+    void list(
+    ) {
+        // Prepare Request
+        var requestURI = "http://" + host + ":" + Integer.toString(port);
+        // Execute Response
+        var response = Unirest.get(requestURI).asJson();
+        // Get File List
+        var files = (JSONArray) response.getBody().getObject().get("files");
+        var filesStrings = StreamSupport.stream(files.spliterator(), false).map(oFile -> {
+            var file = (JSONObject) oFile;
+            return new String[] {
+                file.getString("id"),
+                file.getString("size"),
+                file.getString("name")
+            };
+        }).collect(Collectors.toList());
+        // Compute Column Lengths
+        var columnLengths = new HashMap<Integer, Integer>();
+        for (int i = 0; i < filesStrings.size(); i++) {
+            for (int j = 0; j < filesStrings.get(i).length; j++) {
+                var val = Optional.ofNullable(columnLengths.putIfAbsent(j, 0)).orElse(0);
+                var fscl = filesStrings.get(i)[j].length();
+                if (val.intValue() < fscl) {
+                    columnLengths.put(j, fscl);
+                }
+
+            }
+        }
+        // Define Pretty String
+        final var formatString = new StringBuilder();
+        columnLengths.forEach((_k, len) -> {
+            formatString.append("%" + len.toString() + "s ");
+        });
+        formatString.append("\n");
+        // Pretty Print File List
+        for (var fileStrings : filesStrings) {
+            // Print Info
+            System.out.printf(formatString.toString(), (Object[]) fileStrings);
+        }
+    } 
+    // Define Starting Point
+    @Override
+    public void run() {
+        throw new ParameterException(spec.commandLine(), "Specify a subcommand");
         
-//         File testFile = new File("test.txt");
-//         if (!testFile.exists()) {
-//             System.out.println("no such file");
-//         }
-        
-//         CliBuilder<Runnable> builder = Cli.<Runnable>builder("sponge")
-//             .withDescription("Proof of concept de Replicação Passiva")
-//             .withDefaultCommand(Help.class)
-//             .withCommands(Help.class, Add.class);
-        
-//         builder.withGroup("cu podre")
-//             .withDescription("gay");
+    }
 
-//         var spongeClient = builder.build();
-//         spongeClient.parse(args).run();
-
-//         var fileContent = Files.readAllBytes(testFile.toPath());
-
-//         var res = Unirest.post("http://localhost:9647")
-//             .body(fileContent)
-//             .asString();
-
-//         System.out.println(res.getBody());
-        
-        
-//         System.out.println("client out");
-//     }
-// }
+    public static void main(String[] args) throws IOException {
+        // Initialize from Settings
+        var exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+}
--- src/main/java/br/ufsc/sponge/server/App.java
+++ src/main/java/br/ufsc/sponge/server/App.java
@@ -23,6 +23,7 @@ public class App {
         Logger.info("[Core] Starting application");
         // Initialize from Settings
         Logger.info("[Core] Loading settings");
+
         final SettingsManager settingsManager = SettingsManagerBuilder
             .withYamlFile(
                 Path.of(
@@ -33,15 +34,20 @@ public class App {
             .configurationData(ServerConfiguration.class)
             .useDefaultMigrationService()
             .create();
+            
         Logger.info("[Core] Settings loaded");
         Logger.info("[Core] Node type: {}", StringUtils.capitalize(settingsManager.getProperty(ServerConfiguration.INSTANCE_TYPE).toString()));
         // Initialize Repositories
         Logger.info("[Modules] Initializing modules");
+        
         FileRepository.getInstance().initialize(settingsManager);
         final var clientConnector = new ClientConnector(settingsManager);
+        
         Logger.info("[Modules] Modules loaded");
         Logger.info("[Core] Starting listening");
+        
         clientConnector.startListening();
+        
         Logger.info("[Core] Application ready to receive connections");
     }
 }
